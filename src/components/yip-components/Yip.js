df710540c7f1e4ce1c7407798934f353d408a591
Jayden Rodriguez <02rodriguez.j@gmail.com>
2024-01-20 21:33:03 -0700

    Create check if state has changed for if yip data changed from mount of component

--- src/components/yip-components/Yip.js
+++ src/components/yip-components/Yip.js
@@ -8,35 +8,56 @@ export default function YipNote(props) {
 
     const { yip, change } = props
 
+
+    const [html_value, set_html_value] = useState()
     const [value, setValue] = useState(yip.yip_content)
-    //const [has_value_changed, set_has_value_changed] = useState({initial: false, value: false})
     const [to_change, set_to_change] = useState(false)
 
+    let counts = 0
+
     const quill_change = (e) => {
-        setValue(e)
+        
+        counts = counts + 1
+      
+        if (counts === 2 ){
+            set_html_value(e)
+            setValue(e)
+            counts = 0
+        } else {
+            setValue(e)
+        }
     }
 
     useEffect(() => {
-        //set_has_value_changed({...has_value_changed, initial: false, value: false})
+        set_html_value(value)
     }, [])
-    //console.log('value:      ', value,'has:      ',  has_value_c  hanged)
+
+   
 
     return (
         <StyledContentContainer>
             <StyledContentTopborder />
             <StyledContentbody>
-                <Link to={'/home/navigation-screen'}>
+                <Link to={'/home/navigation-screen'} onClick={() => {
+                    if (html_value !== value){
+                        alert(' differnt! ')
+                    }
+                }}>
                     <button className="button">Back</button>
                 </Link>
                 <br></br>
                 <br></br>
                 {!to_change ?
+
                     <button className='button' onClick={() => set_to_change(!to_change)}>Save Changes</button>
+
                     :
+
                     <>
                         <button className="button" onClick={() => {
                             change(yip.yip_id, { ...yip, yip_content: value })
                             set_to_change(!to_change)
+                            set_html_value(value)
                         }}>Confirm Changes</button>
                         <button className="button" onClick={() => {
                             set_to_change(!to_change)
@@ -45,7 +66,7 @@ export default function YipNote(props) {
                 }
 
                 <StyledQuill>
-                    <ReactQuill theme='snow' value={value} onChange={(e) => { quill_change(e) }} />
+                    <ReactQuill theme="snow" value={value} onChange={(e) => quill_change(e)} />
                 </StyledQuill>
             </StyledContentbody>
         </StyledContentContainer>
